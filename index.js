8cc931fe1ab9d6f05a15b5254d55156ed31e424c
ummarmtp <ummarmtp@gmail.com>
2024-07-15 15:41:29 +0400

    "final mongodb url"

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 // MongoDB connection
-  //mongoose.connect("mongodb+srv://ummarrahil:06031998Rahil@cluster0.7baglhg.mongodb.net/device?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-  mongoose.connect("mongodb://127.0.0.1:27017/device").then(()=>{
+  mongoose.connect("mongodb+srv://ummarrahil:06031998Rahil@cluster0.7baglhg.mongodb.net/device?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+  //mongoose.connect("mongodb://127.0.0.1:27017/device").then(()=>{
   console.log("mongodb connected")
   initializeData();
 }).catch((error)=>console.log(error));
